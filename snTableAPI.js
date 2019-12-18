19044188d6ffb22f2ddfcf12d8af8fd8f41c64c5
Vitalii Perminov <vperm@softserveinc.com>
2019-12-18 09:26:23 +0200

    new

--- snTableAPI.js
+++ snTableAPI.js
@@ -3,36 +3,52 @@ const _USER_ID = "admin";
 const _PASSWORD = "kk67HuDifOMC";
 
 //const email2Find = "charlie.whitherspoon@example.com";
-const email2Find = "sabrina.deppert@example.com";
+//const email2Find = "sabrina.deppert@example.com";
 //const email2Find = "zane.sulikowski@example.com";
-let sysparmQuery = "?sysparm_query=email=" + email2Find;
-let sysparmQueryIncident = "?sysparm_query=email=" + email2Find;
 
-// Request data
+const email2Find = "david.miller@example.com";
 let fields2Return = "name,user_name,email,sys_id,active";
-let sysparmFields = "&sysparm_fields=" + fields2Return;
 
-let fields2ReturnIncident = "name,user_name,email,sys_id,active";
-let sysparmFieldsIncident = "&sysparm_fields=" + fields2Return;
+const sysId2FindIncident = "77ad8176731313005754660c4cf6a7de";
+let fields2ReturnIncident = "number,short_description,sys_created_on";
 
+//opened_by.value=77ad8176731313005754660c4cf6a7de
+//number,short_description
 
-let response = "";
+//let fields2ReturnIncident = "name,short_description";
+//let sysparmFieldsIncident = "&sysparm_fields=" + fields2Return;
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let request = new XMLHttpRequest();
 
+let response = "";
 // As Andrii said, replaced above the request itself
 request.onload = function() {
     response = JSON.parse(request.responseText);
-    console.log(JSON.stringify(response.result[0], null, '\t'));
+    //console.log(JSON.stringify(response.result[0], null, '\t'));
+    response.result.forEach(element => {
+        console.log(JSON.stringify(element, null, '\t'));
+    });
 }
 
+/*
+// Request for sys_id by email
+// Prepare request data
+let sysparmQuery = "?sysparm_query=email=" + email2Find;
+let sysparmFields = "&sysparm_fields=" + fields2Return;
 // Request
-request.open('GET', _INSTANCE + 'sys_user' + sysparmQuery + sysparmFields, true, _USER_ID, _PASSWORD);
+request.open('GET', _INSTANCE + 'sys_user' + sysparmQuery + sysparmFields, false, _USER_ID, _PASSWORD);
+request.send();
+*/
 
+// Request for incidents by creator's sys_id
+// Prepare request data
+let sysparmQueryIncident = "?sysparm_query=opened_by.value=" + sysId2FindIncident;
+let sysparmFieldsIncident = "&sysparm_fields=" + fields2ReturnIncident;
+request.open('GET', _INSTANCE + 'incident' + sysparmQueryIncident + sysparmFieldsIncident, true, _USER_ID, _PASSWORD);
 request.send();
 
-console.log(request.responseText)
+//request.open('GET', _INSTANCE + 'incident' + sysparmQueryIncident + sysparmFieldsIncident, true, _USER_ID, _PASSWORD);
 
 // One way to loop
 // response.result.forEach(element => {
