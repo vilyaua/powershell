aca66fcbfb124169a4f290f6f38510aae3c6e2e3
Vitalii Perminov <vperm@softserveinc.com>
2019-12-18 12:30:21 +0200

    new

--- snTableAPI.js
+++ snTableAPI.js
@@ -2,15 +2,18 @@ const _INSTANCE = "https://dev62099.service-now.com/api/now/table/";
 const _USER_ID = "admin";
 const _PASSWORD = "kk67HuDifOMC";
 
-//const email2Find = "charlie.whitherspoon@example.com";
-//const email2Find = "sabrina.deppert@example.com";
-//const email2Find = "zane.sulikowski@example.com";
+const email2Find = "don.goodliffe@example.com"; //9ee1b13dc6112271007f9d0efdb69cd0
+//const email2Find = "guest@example.com"; //5136503cc611227c0183e96598c4f706
+//const email2Find = "itil@example.com"; //681b365ec0a80164000fb0b05854a0cd
+//const email2Find = "admin@example.com"; //6816f79cc0a8016401c5a33be04be441
 
-const email2Find = "david.miller@example.com";
-let fields2Return = "name,user_name,email,sys_id,active";
+let fields2Return = "name,user_name,email,sys_id";
 
-const sysId2FindIncident = "77ad8176731313005754660c4cf6a7de";
-let fields2ReturnIncident = "number,short_description,sys_created_on";
+const sysId2FindIncident = "9ee1b13dc6112271007f9d0efdb69cd0";
+let fields2ReturnIncident = "number,short_description,sys_created_on,opened_by";
+
+const sysId2FindChangeRequest = "9ee1b13dc6112271007f9d0efdb69cd0";
+let fields2ReturnChangeRequest = "number,short_description,sys_created_on,opened_by";
 
 //opened_by.value=77ad8176731313005754660c4cf6a7de
 //number,short_description
@@ -29,33 +32,49 @@ request.onload = function() {
     response.result.forEach(element => {
         console.log(JSON.stringify(element, null, '\t'));
     });
+    console.log("Output Finished")
 }
 
 /*
 // Request for sys_id by email
 // Prepare request data
-let sysparmQuery = "?sysparm_query=email=" + email2Find;
-let sysparmFields = "&sysparm_fields=" + fields2Return;
+let sysparmQuery = "sysparm_query=email=" + email2Find;
+let sysparmFields = "sysparm_fields=" + fields2Return;
 // Request
-request.open('GET', _INSTANCE + 'sys_user' + sysparmQuery + sysparmFields, false, _USER_ID, _PASSWORD);
+request.open('GET', _INSTANCE + 'sys_user' + '?' + sysparmQuery + '&' + sysparmFields, false, _USER_ID, _PASSWORD);
 request.send();
 */
 
-// Request for incidents by creator's sys_id
+/*
+// Request for Incidents by creator's sys_id
 // Prepare request data
-let sysparmQueryIncident = "?sysparm_query=opened_by.value=" + sysId2FindIncident;
-let sysparmFieldsIncident = "&sysparm_fields=" + fields2ReturnIncident;
-request.open('GET', _INSTANCE + 'incident' + sysparmQueryIncident + sysparmFieldsIncident, true, _USER_ID, _PASSWORD);
+let sysparmQueryIncident = "sysparm_query=opened_by=" + sysId2FindIncident;
+let sysparmFieldsIncident = "sysparm_fields=" + fields2ReturnIncident;
+request.open('GET', _INSTANCE + 'incident' + '?' + sysparmQueryIncident + '&' + sysparmFieldsIncident, true, _USER_ID, _PASSWORD);
+request.send();
+*/
+
+
+// Request for ChangeRequests by creator's sys_id
+// Prepare request data
+let sysparmQueryChangeRequest = "sysparm_query=opened_by=" + sysId2FindChangeRequest;
+let sysparmFieldsChangeRequest = "sysparm_fields=" + fields2ReturnChangeRequest;
+request.open('GET', _INSTANCE + 'change_request' + '?' + sysparmQueryChangeRequest + '&' + sysparmFieldsChangeRequest, true, _USER_ID, _PASSWORD);
 request.send();
 
-//request.open('GET', _INSTANCE + 'incident' + sysparmQueryIncident + sysparmFieldsIncident, true, _USER_ID, _PASSWORD);
+
+// ?Doesn't work - WHY?
+// const sn=require("servicenow-rest-api");
+// const ServiceNow=new sn('https://dev62099.service-now.com','admin','kk67HuDifOMC');
+// ServiceNow.Authenticate();
+// ServiceNow.getSampleData('change_request',(res)=>{    // 
+//     console.log(res);
+// })
 
 // One way to loop
 // response.result.forEach(element => {
 //     console.log(element.email);
-// });
-
-//console.log(JSON.stringify(response.result[0]));
+// })
 
 // Second way to loop
 // for (let elem in response.result) {
@@ -65,13 +84,3 @@ request.send();
 //         userId2Find = response.result[elem].userid;
 //     }
 // }
-// console.log("\nsys_id: ", sys_Id2Find);
-// console.log("\nuserid: ", userId2Find)
-
-// ?Doesn't work - WHY?
-// const sn=require("servicenow-rest-api");
-// const ServiceNow=new sn('https://dev62099.service-now.com','admin','kk67HuDifOMC');
-// ServiceNow.Authenticate();
-// ServiceNow.getSampleData('change_request',(res)=>{    // 
-//     console.log(res);
-// });
