3cbfd2cc5f905c3710d2d5ab61f101bdde0d75f1
Vitalii Perminov <vperm@softserveinc.com>
2019-12-27 09:29:31 +0200

    added new main file

--- restApiClientUser.js
+++ restApiClientUser.js
@@ -1,3 +1,24 @@
+var tableApiRequestBuilder = require('./TableApiRequestBuilder');
+
 function restApiClientUser() {
+    this.host;
+    this.user;
+    this.pass;
+
+    this.createUser = function() {
+
+    }
+
+    this.updateUser = function() {
+
+    }
+
+    this.getUserById = function() {
+
+    }
+
+    this.deleteUserById = function() {
+
+    }
 
 }
