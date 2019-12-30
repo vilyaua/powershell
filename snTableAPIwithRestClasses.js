4871ecb1a8ed70d02ea5688dd09ebcf4e482a28b
Vitalii <vilyaua@gmail.com>
2019-12-30 05:45:05 +0200

    cleaned up

--- snTableAPIwithRestClasses.js
+++ snTableAPIwithRestClasses.js
@@ -1,17 +1,19 @@
 var restApiClientUser = require('./restApiClientUser');
 
-// Leave any sample below uncommented to find data by this particular Email
-//const userEmail = "don.goodliffe@example.com"; //9ee1b13dc6112271007f9d0efdb69cd0
-//const userEmail = "guest@example.com"; //5136503cc611227c0183e96598c4f706
-const userEmail = "itil@example.com"; //681b365ec0a80164000fb0b05854a0cd
-//const userEmail = "admin@example.com"; //6816f79cc0a8016401c5a33be04be4
-
 var userInitData = {
-    user_name: "Vilya", // This field needs to be unique
+    user_name: "Vilya 8", // This field needs to be unique
     email: "atLast@email.com",
     mobile_phone: "+380676360948",
     country: "Ukraine"
 }
 
 var client = new restApiClientUser();
-client.createUser(userInitData);
+client.createUser(userInitData)
+    .then (function(userData) {
+        var user = userData.result;
+        var sysId = user.sys_id;
+        console.log('User created: ' + JSON.stringify(user, null, '    '));
+    })
+    .catch(function(err) {
+        console.error(err);
+    })
